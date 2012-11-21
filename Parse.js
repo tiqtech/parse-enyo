98a6540589f4dfd3bf49b373e30bb5149bf7fac1
ryanjduffy <ryan@thisduffylife.com>
2012-11-20 22:02:28 -0600

    Revamped for Enyo v2, updated Parse REST API, and added support for Parse Cloud Code (via run() method)

--- Parse.js
+++ Parse.js
@@ -1,77 +1,142 @@
-var _Parse = {
-	name:"com.parse.RestClient",
-	kind:"Component",
-	published:{
-		applicationId:"",
-		//clientKey:"",
-		masterKey:"",
-		className:""
-	},
-	events:{
-		onAdd:"",
-		onUpdate:"",
-		onGet:"",
-		onRemove:"",
-		onSearch:"",
-		onError:""
-	},
-	parse:{
-		host:"https://api.parse.com",
-		path:"1/classes"
-	},	
-	components:[
-        {name:"ws", kind:"WebService", contentType:"application/json", onSuccess:"callSuccess", onFailure:"callFailure"}
-    ],
-    create:function() {
-    	this.inherited(arguments);
-    	this.applicationIdChanged();
-    	this.masterKeyChanged();
+enyo.kind({
+    name:"Parse.RestClient",
+    kind:"Component",
+    published:{
+        applicationId:"",
+        key:""
     },
-    applicationIdChanged:function() {
-    	this.$.ws.setUsername(this.applicationId);
+    events:{
+        onAdd:"",
+        onUpdate:"",
+        onGet:"",
+        onRemove:"",
+        onSearch:"",
+        onError:""
     },
-    masterKeyChanged:function() {
-    	this.$.ws.setPassword(this.masterKey);
+    parse:{
+        host:"api.parse.com",
+        version:"1"
     },
-    setUrl:function(id) {
-    	var p = [this.parse.host,this.parse.path,this.className];
-    	if(id) p.push(id);
-    	
-    	this.$.ws.setUrl(p.join("/"));
+    getUrl:function(id, endpoint, className) {
+        var p = ["https://",this.parse.host,this.parse.version,endpoint,className];
+        if(id) {
+            p.push(id);
+        }
+        
+        return p.join("/");
     },
-    call:function(method, data, successEvent) {
-    	this.$.ws.setMethod(method);
-    	var request = this.$.ws.call(data && enyo.json.stringify(data));
-    	request.__callback = successEvent;
+    call:function(config) {
+        config = enyo.mixin({
+            endpoint:"classes",
+            method:"GET"
+        }, config);
+        
+        var x = new enyo.Ajax({
+            method:config.method,
+            url:this.getUrl(config.id, config.endpoint, config.className),
+            cacheBust:false,
+            contentType:"application/json",
+            headers:{
+                "X-Parse-Application-Id":this.applicationId,
+                "X-Parse-REST-API-Key":this.key
+            }
+        });
+        
+        x.go(config.data);
+        x.error(this, function(sender, response) {
+            this.doError({response:response});
+            if(config.callback) {
+                config.callback(sender, {
+                    error:response
+                });
+            }
+        });
+        
+        if(config.event) {
+            x.response(this, function(sender, response) {
+                var stop = false;
+                var e = {
+                    response:response,
+                    stop:function() {
+                        stop = true;
+                    }
+                };
+                
+                this[config.event](e);
+                
+                if(stop) {
+                    x.fail();
+                } else {
+                    return response;
+                }
+            });
+        }
+        
+        if(config.callback) {
+            x.response(function(sender, response) {
+                config.callback(sender, {response:response});
+            });
+        }
     },
-    callSuccess:function(source, response, request) {
-    	this[request.__callback](response);
+    add:function(className, o, callback) {
+        this.call({
+            className:className,
+            method:"POST",
+            data:enyo.isString(o) ? o : enyo.json.stringify(o),
+            event:"doAdd",
+            callback:callback
+        });
     },
-    callFailure:function(source, response, request) {
-    	enyo.log("failure!")
-    	this.doError(request);
+    get:function(className, id, callback) {
+        this.call({
+            className:className,
+            method:"GET",
+            id:id,
+            event:"doGet",
+            callback:callback
+        });
     },
-    add:function(o) {
-    	this.setUrl();
-    	this.call("POST", o, "doAdd");
+    update:function(className, o, callback) {
+        this.call({
+            className:className,
+            method:"PUT",
+            id:o.objectId,
+            data:enyo.isString(o) ? o : enyo.json.stringify(o),
+            event:"doUpdate",
+            callback:callback
+        });
     },
-    get:function(id) {
-    	this.setUrl(id);
-    	this.call("GET", undefined, "doGet");
+    search:function(className, query, callback) {
+        for(var k in query) {
+            if(query[k] instanceof Object) {
+                query[k] = enyo.json.stringify(query[k]);
+            }
+        }
+        
+        this.call({
+            className:className,
+            method:"GET",
+            data:query,
+            event:"doSearch",
+            callback:callback
+        });
     },
-    update:function(id, o) {
-    	this.setUrl(id);
-    	this.call("PUT", o, "doUpdate");
+    remove:function(className, id, callback) {
+        this.call({
+            className:className,
+            method:"DELETE",
+            id:id,
+            event:"doRemove",
+            callback:callback
+        });
     },
-    search:function(query) {
-    	this.setUrl();
-    	this.$.ws.setUrl(this.$.ws.getUrl() + "?where=" + enyo.json.stringify(query));
-    	this.call("GET", undefined, "doSearch");
-    },
-    remove:function(id) {
-    	this.setUrl(id);
-    	this.call("DELETE", undefined, "doRemove");
+    run:function(name, args, callback) {
+        this.call({
+            endpoint:"functions",
+            className:name,
+            method:"POST",
+            data:args ? enyo.json.stringify(args) : "{}",
+            callback:callback
+        });
     }
-};
-
-enyo.kind(_Parse);
+});
